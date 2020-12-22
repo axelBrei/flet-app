d44075b5edb0695eed7eae6150a78da4402954b0
Axel Breiterman <axelbrei@AxelBreiterman.local>
2020-12-21 22:44:36 -0300

    Remove log and unnecessary function

--- src/components/ui/Map/index.web.js
+++ src/components/ui/Map/index.web.js
@@ -38,31 +38,6 @@ const Map = ({google, markers, minMarkerAnimation, ...props}) => {
     }
   }, [filteredMarkers, google, mapRef]);
 
-  const onLoadMarker = useCallback((marker) => {
-    const customIcon = (opts) =>
-      Object.assign(
-        {
-          path:
-            'M7.8,1.3L7.8,1.3C6-0.4,3.1-0.4,1.3,1.3c-1.8,1.7-1.8,4.6-0.1,6.3c0,0,0,0,0.1,0.1 l3.2,3.2l3.2-3.2C9.6,6,9.6,3.2,7.8,1.3C7.9,1.4,7.9,1.4,7.8,1.3z M4.6,5.8c-0.7,0-1.3-0.6-1.3-1.4c0-0.7,0.6-1.3,1.4-1.3 c0.7,0,1.3,0.6,1.3,1.3 C5.9,5.3,5.3,5.9,4.6,5.8z',
-          fillColor: '#f00',
-          fillOpacity: 1,
-          strokeColor: '#000',
-          strokeWeight: 1,
-          scale: 3.5,
-        },
-        opts,
-      );
-
-    marker.setIcon(
-      customIcon({
-        fillColor: 'green',
-        strokeColor: 'white',
-      }),
-    );
-    return markerLoadHandler(marker, place);
-  }, []);
-
-  console.log(google.maps.SymbolPath);
   return (
     <View style={[styles.container, props.style]}>
       <GoogleMap

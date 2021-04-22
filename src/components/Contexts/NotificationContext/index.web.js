28910639a4a98976e98ca8f896181f7fe16ee2b7
Axel Breiterman <axelbreiterman@MacBook-Pro-de-Axel.local>
2021-04-22 17:20:43 -0300

    Add splash to web

--- src/components/Contexts/NotificationContext/index.web.js
+++ src/components/Contexts/NotificationContext/index.web.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 export default ({children}) => {
+  useEffect(() => {
+    // TODO: remove splash screen
+    const splash = document.getElementById('splash');
+    splash.style.display = 'none';
+  }, []);
   return <>{children}</>;
 };
